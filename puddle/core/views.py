760209b1f90638caed02f8c902359142c69b9228
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-07 21:27:52 +0100

    added a contact template
    added a header and footer to the base html

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "core/index.html") 
+
+def contact(request):
+    # Links to the contact page
+    return render(request, "core/contact.html")
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index, contact
 
 urlpatterns = [
     path('', index, name="index"), # empty because it is the homepage
+    path('contact/', contact, name="contact"), # contact template
     path('admin/', admin.site.urls),
 ]
