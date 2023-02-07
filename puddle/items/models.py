7c4b09b0ff41e3d4624dbd30107a562996c42a47
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-07 22:00:10 +0100

    made migrations to start the categories database

--- puddle/items/models.py
+++ puddle/items/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    # This simulates an item's category
+    name = models.CharField(max_length=255)
+
+    class Meta():
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
