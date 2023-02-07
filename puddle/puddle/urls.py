13a0c3db4c071fcec17794c887b4d459a8ad89d8
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-07 18:00:13 +0100

    added the home index template to utls

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from core.views import index
+
 urlpatterns = [
+    path('', index, name="index"), # empty because it is the homepage
     path('admin/', admin.site.urls),
 ]
