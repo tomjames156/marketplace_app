321e5cd2baba70bd4107e19356ab74164ef6dc0a
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-08 18:36:33 +0100

    added an item model and did the mihgrations

--- puddle/items/models.py
+++ puddle/items/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -10,3 +11,14 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+class Item(models.Model):
+    # This represents an item on the website
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
